450475be3a659a87c45a3244bb032669452031d1
Freddy Quiroga <freddybalderrama.fa@gmail.com>
2021-07-28 16:53:33 -0400

    primer commit

---
+++ useCouter.js
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+export const useCouter = ( initialState = 10 ) => {
+  
+  
+  const [counter, setCounter] = useState( initialState );
+
+  const reset = () => {
+    setCounter( initialState )
+  }
+  // const increment = ( factor = 1) =>{
+  //   setstate( state + factor )
+  // }
+  // const decrement = ( factor = 1 ) =>{
+  //   setstate( state - factor )
+  // }
+  const increment = () =>{
+    setCounter( counter + 1 )
+  }
+  const decrement = () =>{
+    setCounter( counter - 1 )
+  }
+  return {
+    counter,
+    increment,
+    decrement,
+    reset
+  };
+}
